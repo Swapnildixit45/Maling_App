0a4b34bb482cdd05cbd0ba4dce15d714dbe0bec8
Swapnildixit45 <57413050+Swapnildixit45@users.noreply.github.com>
2023-03-03 01:09:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,60 +1,62 @@
-// VERIFY EMAILS
-
+//---------validating emails--------------
 let upload = document.getElementById('upload');
-upload.addEventListener('change',() => {
+upload.addEventListener('change', () => {
     let fr = new FileReader();
     fr.readAsText(upload.files[0]);
-    fr.onload = () =>{
+    fr.onload = function () {
+
         let Arr = fr.result.split(/\r?\n|\n/).map(e => {
             return e.split(',');
         });
-
         Window.valNo = 0;
         let invalNo = 0;
         Window.valMail = [];
         Arr.forEach(e => {
             let em = String(e);
             let m = e.map(e => {
-                return `<td>${e}</td>`;
+                return `<td>${e}</td>`; 
             })
-            let creEle = document.createElement("tr");
+            let creEle = document.createElement("tr"); // table row
             creEle.innerHTML = m;
-            if(em != "") {
-                if(em.charAt(em.length - 4) == '.') {
+            if (em != "") { // so that blank row will not be printed as well as counted
+                // if (em.indexOf('@') != 0) {
+                //     document.querySelector("table#val").appendChild(creEle);
+                //     return false;
+                // }
+                if (em.charAt(em.length - 4) == '.') {
                     document.querySelector("table#val").appendChild(creEle);
-                    Window.valMail.psuh(em);
+                    Window.valMail.push(em);
                     Window.valNo = Window.valNo + 1;
                     return false;
                 }
-
                 else if (em.charAt(em.length - 3) == '.') {
                     document.querySelector("table#val").appendChild(creEle);
                     Window.valMail.push(em);
                     Window.valNo = Window.valNo + 1;
                     return false;
                 }
-                else{
+                else {
                     document.querySelector("table#inval").appendChild(creEle);
-                    invalNo = inValNo + 1;
+                    invalNo = invalNo + 1;
+                    
                     return false;
                 }
             }
         });
 
-        document.querySelector("#valCount").innerHTML = Window.valNo;
-        document.querySelector('#invalCount').innerHTML = invalNo;
+        document.querySelector('#valCount').innerHTML = Window.valNo;
+        document.querySelector('#invalCount').innnerHTML = invalNo;
     };
 });
+//-----------sending emails---------------
 
-// SEND EMAILS
-
-sendEmail = () => {
+function sendEmail() {
     Email.send({
         Host: "smtp.elasticemail.com",
-        Username: "", 
-        Password: "",  
-        To: "",
-        From: "",
+        Username: "swapnildixit515@gmail.com", 
+        Password: "Swapnil@123",  
+        To: "swapnildixit515@gmail.com",
+        From: "swapnildixit515@gmail.com",
         Subject: document.querySelector('#subject').value,
         Body: document.getElementById('msg').value
     }).then(
@@ -64,4 +66,3 @@ sendEmail = () => {
     console.log(document.getElementById('msg').innerHTML);
     console.log(document.getElementById('msg').innerText);
 }
-
